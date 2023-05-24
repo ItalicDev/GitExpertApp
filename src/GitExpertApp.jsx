d85160cbb9ae48ed47ffe0b3d810a7b51c66c188
Alejandro It <italicdev@gmail.com>
2023-05-24 08:04:43 -0400

    cambios pequeños

--- src/GitExpertApp.jsx
+++ src/GitExpertApp.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useState } from 'react'
+
 
 export const GitExpertApp = () => {
-  return (
+  
+    const [categories, setcategories] = useState(["One Punch", "Dragon ball"])
+    const addCategory = () => {
+        setcategories([...categories, "Valorant"])
+    } 
+    return (
     <>
-         <h1>GitExpertApp</h1>
+        <h1>Tittle</h1>
+        <input></input>
+        <button onClick={addCategory}>
+            Add
+        </button>
+        <ol>
+            {categories.map(cat => {
+                return <li key={cat}>{cat}</li>
+            })}
+        </ol>
     </>
   )
 }
+
